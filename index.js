2c67f9882412a9aa941af09f73cd690677bd3e12
khairulistiyak <khairulistiyakweb@gmail.com>
2024-02-21 23:54:41 +0600

    mongo db start

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const cors = require("cors");
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// innovateLab
+// 6igjU2X7ncFThUH4
+
+app.get("/", (req, res) => {
+  res.send("innovalab server running ");
+});
+
+app.listen(port, () => {
+  console.log(`innova server running on port ${port}`);
+});
